46a15aa513e4af86dfe5a51765b05072f91553a9
bevous <43658573+bevous@users.noreply.github.com>
2019-03-03 14:49:44 -0600

    verify the answers

--- Cards/Cards/Form1.Designer.cs
+++ Cards/Cards/Form1.Designer.cs
@@ -45,18 +45,20 @@
             // 
             // cardOnePictureBox
             // 
-            this.cardOnePictureBox.Location = new System.Drawing.Point(12, 57);
+            this.cardOnePictureBox.Location = new System.Drawing.Point(14, 71);
+            this.cardOnePictureBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cardOnePictureBox.Name = "cardOnePictureBox";
-            this.cardOnePictureBox.Size = new System.Drawing.Size(227, 337);
+            this.cardOnePictureBox.Size = new System.Drawing.Size(255, 421);
             this.cardOnePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.cardOnePictureBox.TabIndex = 0;
             this.cardOnePictureBox.TabStop = false;
             // 
             // ShffleButton
             // 
-            this.ShffleButton.Location = new System.Drawing.Point(760, 16);
+            this.ShffleButton.Location = new System.Drawing.Point(855, 20);
+            this.ShffleButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ShffleButton.Name = "ShffleButton";
-            this.ShffleButton.Size = new System.Drawing.Size(131, 35);
+            this.ShffleButton.Size = new System.Drawing.Size(147, 44);
             this.ShffleButton.TabIndex = 4;
             this.ShffleButton.Text = "Shuffle";
             this.ShffleButton.UseVisualStyleBackColor = true;
@@ -64,52 +66,59 @@
             // 
             // verifyAnswerButton
             // 
-            this.verifyAnswerButton.Location = new System.Drawing.Point(760, 403);
+            this.verifyAnswerButton.Location = new System.Drawing.Point(855, 504);
+            this.verifyAnswerButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.verifyAnswerButton.Name = "verifyAnswerButton";
-            this.verifyAnswerButton.Size = new System.Drawing.Size(131, 35);
+            this.verifyAnswerButton.Size = new System.Drawing.Size(147, 44);
             this.verifyAnswerButton.TabIndex = 5;
             this.verifyAnswerButton.Text = "Verify";
             this.verifyAnswerButton.UseVisualStyleBackColor = true;
+            this.verifyAnswerButton.Click += new System.EventHandler(this.verifyAnswerButton_Click);
             // 
             // expressionTextBox
             // 
-            this.expressionTextBox.Location = new System.Drawing.Point(296, 409);
+            this.expressionTextBox.Location = new System.Drawing.Point(333, 511);
+            this.expressionTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.expressionTextBox.Name = "expressionTextBox";
-            this.expressionTextBox.Size = new System.Drawing.Size(382, 22);
+            this.expressionTextBox.Size = new System.Drawing.Size(429, 26);
             this.expressionTextBox.TabIndex = 6;
+            this.expressionTextBox.TextChanged += new System.EventHandler(this.expressionTextBox_TextChanged);
             // 
             // expressionLabel
             // 
             this.expressionLabel.AutoSize = true;
-            this.expressionLabel.Location = new System.Drawing.Point(88, 412);
+            this.expressionLabel.Location = new System.Drawing.Point(99, 515);
             this.expressionLabel.Name = "expressionLabel";
-            this.expressionLabel.Size = new System.Drawing.Size(134, 17);
+            this.expressionLabel.Size = new System.Drawing.Size(150, 20);
             this.expressionLabel.TabIndex = 7;
             this.expressionLabel.Text = "Enter an expression";
             // 
             // cardTwoPictureBox
             // 
-            this.cardTwoPictureBox.Location = new System.Drawing.Point(245, 57);
+            this.cardTwoPictureBox.Location = new System.Drawing.Point(276, 71);
+            this.cardTwoPictureBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cardTwoPictureBox.Name = "cardTwoPictureBox";
-            this.cardTwoPictureBox.Size = new System.Drawing.Size(227, 337);
+            this.cardTwoPictureBox.Size = new System.Drawing.Size(255, 421);
             this.cardTwoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.cardTwoPictureBox.TabIndex = 8;
             this.cardTwoPictureBox.TabStop = false;
             // 
             // cardThreePictureBox
             // 
-            this.cardThreePictureBox.Location = new System.Drawing.Point(478, 57);
+            this.cardThreePictureBox.Location = new System.Drawing.Point(538, 71);
+            this.cardThreePictureBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cardThreePictureBox.Name = "cardThreePictureBox";
-            this.cardThreePictureBox.Size = new System.Drawing.Size(227, 337);
+            this.cardThreePictureBox.Size = new System.Drawing.Size(255, 421);
             this.cardThreePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.cardThreePictureBox.TabIndex = 9;
             this.cardThreePictureBox.TabStop = false;
             // 
             // cardFourPictureBox
             // 
-            this.cardFourPictureBox.Location = new System.Drawing.Point(711, 57);
+            this.cardFourPictureBox.Location = new System.Drawing.Point(800, 71);
+            this.cardFourPictureBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cardFourPictureBox.Name = "cardFourPictureBox";
-            this.cardFourPictureBox.Size = new System.Drawing.Size(227, 337);
+            this.cardFourPictureBox.Size = new System.Drawing.Size(255, 421);
             this.cardFourPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.cardFourPictureBox.TabIndex = 10;
             this.cardFourPictureBox.TabStop = false;
@@ -117,17 +126,17 @@
             // resultsLabel
             // 
             this.resultsLabel.AutoSize = true;
-            this.resultsLabel.Location = new System.Drawing.Point(604, 16);
+            this.resultsLabel.Location = new System.Drawing.Point(680, 20);
             this.resultsLabel.Name = "resultsLabel";
-            this.resultsLabel.Size = new System.Drawing.Size(46, 17);
+            this.resultsLabel.Size = new System.Drawing.Size(51, 20);
             this.resultsLabel.TabIndex = 11;
             this.resultsLabel.Text = "label2";
             // 
             // cardsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(953, 450);
+            this.ClientSize = new System.Drawing.Size(1072, 562);
             this.Controls.Add(this.resultsLabel);
             this.Controls.Add(this.cardFourPictureBox);
             this.Controls.Add(this.cardThreePictureBox);
@@ -137,6 +146,7 @@
             this.Controls.Add(this.verifyAnswerButton);
             this.Controls.Add(this.ShffleButton);
             this.Controls.Add(this.cardOnePictureBox);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "cardsForm";
             this.Text = "Cards";
             this.Load += new System.EventHandler(this.cardsForm_Load);
--- Cards/Cards/Form1.cs
+++ Cards/Cards/Form1.cs
@@ -10,12 +10,20 @@ using System.Windows.Forms;
 
 namespace Cards
 {
+    using System.Drawing.Drawing2D;
+
     public partial class cardsForm : Form
     {
         private List<Card> Deck { get; set; } = new List<Card>();
 
         private static readonly Bitmap CardBack = new Bitmap(@"images\backCard.png");
 
+        Stack<Card> currentCards = new Stack<Card>();
+
+        Stack<Card> usedCards = new Stack<Card>();
+
+        private int numberOfShuffles = 0;
+        
         public cardsForm()
         {
             this.InitializeComponent();
@@ -23,23 +31,113 @@ namespace Cards
 
         private void MakeDeck()
         {
+            var cardValue = 1;
+            var suitType = 1;
             for (var imageNumber = 1; imageNumber <= 52; imageNumber++)
             {
                 string fileName = $@"images\{imageNumber:G}.png";
                 var image = new Bitmap(fileName);
-                this.Deck.Add(new Card { Value = imageNumber, Suit = "spade", CardFace = image });
+                if (cardValue > 13)
+                {
+                    cardValue = 1;
+                    suitType++;
+                }//else, DoNothing();
+
+                if (suitType == 1)
+                {
+                    this.Deck.Add(new Card { Value = cardValue, Suit = "Spade", CardFace = image });
+                }
+                else if (suitType == 2)
+                {
+                    this.Deck.Add(new Card { Value = cardValue, Suit = "Heart", CardFace = image });
+                }
+                else if (suitType == 3)
+                {
+                    this.Deck.Add(new Card { Value = cardValue, Suit = "Diamond", CardFace = image });
+                }
+                else if (suitType == 4)
+                {
+                    this.Deck.Add(new Card { Value = cardValue, Suit = "Club", CardFace = image });
+                }
             }
         }
 
         private void ShffleButton_Click(object sender, EventArgs e)
         {
-            this.MakeDeck();
+            if (this.numberOfShuffles == 12)
+            {
+                this.numberOfShuffles = 0;
+                this.currentCards.Clear();
+                this.usedCards.Clear();
+            }
+           var numberRandom = new Random();
+           this.currentCards.Clear();
+           while (this.currentCards.Count < 4)
+           {
+               int cardChosen = numberRandom.Next(0, 51);
+               if (!this.currentCards.Contains(this.Deck[cardChosen]) && !this.usedCards.Contains(this.Deck[cardChosen]))
+               {
+                    this.currentCards.Push(this.Deck[cardChosen]);
+               } // else, card is already used doNothing();
+           }
+
+           this.numberOfShuffles++;
+
+            this.cardOnePictureBox.Image = this.currentCards.Peek().CardFace;
+           this.usedCards.Push(this.currentCards.Pop());
+           this.cardTwoPictureBox.Image = this.currentCards.Peek().CardFace;
+           this.usedCards.Push(this.currentCards.Pop());
+           this.cardThreePictureBox.Image = this.currentCards.Peek().CardFace;
+           this.usedCards.Push(this.currentCards.Pop());
+           this.cardFourPictureBox.Image = this.currentCards.Peek().CardFace;
+           this.usedCards.Push(this.currentCards.Pop());
         }
 
         private void cardsForm_Load(object sender, EventArgs e)
         {
             this.MakeDeck();
-            this.cardOnePictureBox.Image=this.ca
+            this.cardOnePictureBox.Image = CardBack;
+            this.cardTwoPictureBox.Image = CardBack;
+            this.cardThreePictureBox.Image = CardBack;
+            this.cardFourPictureBox.Image = CardBack;
+            this.verifyAnswerButton.Hide();
+        }
+
+        private void verifyAnswerButton_Click(object sender, EventArgs e)
+        {
+            Stack<char> operatorStack = new Stack<char>();
+
+            Stack<int> numberStack = new Stack<int>();
+        }
+
+        private void expressionTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var validCharacters = "0123456789+-*/() ";
+            validCharacters.ToCharArray();
+           
+            var isvalid = false;
+            foreach (var character in this.expressionTextBox.Text)
+            {
+                var index = 0;
+                while (index < validCharacters.Length && !(isvalid = character == validCharacters[index]))
+                {
+                    index++;
+                }
+                if (!isvalid)
+                {
+                    this.expressionTextBox.BackColor = Color.Red;
+                    this.verifyAnswerButton.Hide();
+                    return;
+                }
+                else
+                {
+                    this.expressionTextBox.BackColor = Color.White;
+                    this.verifyAnswerButton.Show();
+                }
+                
+            }
+
+
         }
     }
 }
